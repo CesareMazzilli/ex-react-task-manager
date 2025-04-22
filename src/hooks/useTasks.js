6a020d4c122112a345252761edde3df1a0a3bb38
CesareMazzilli <cesaremazzilli98@gmail.com>
2025-04-22 18:25:57 +0200

    first commit

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+const { VITE_API_URL } = import.meta.env;
+
+export default function useTasks(){
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch(`${VITE_API_URL}/tasks`)
+           .then(res => res.json())
+           .then(data => setTasks(data))
+           .catch(error => console.error(error));
+    }, []);
+
+    const addTask = async newTask => {
+        const response = await fetch(`${VITE_API_URL}/tasks`, {
+            method: 'POST',
+            headers: { "Content-type": "application/json"},
+            body: JSON.stringify(newTask)
+        });
+        const { success, message, task } = await response.json();
+        if(!success) throw new Error(message);
+
+        setTasks(prev => [...prev, task]);
+    }
+
+    const removeTask = () => {}
+
+    const updateTask = () => {}
+
+    return { tasks, addTask, removeTask, updateTask }
+}
---
+++ src/pages/AddTask.jsx
@@ -0,0 +1,80 @@
+import { useState, useRef, useMemo, useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+
+export default function AddTask(){
+    const { addTask } = useContext(GlobalContext);
+
+    const [taskTitle, setTaskTitle] = useState("");
+    const descriptionRef = useRef();
+    const statusRef = useRef();
+
+    const taskTitleError = useMemo(() => {
+        if(!taskTitle.trim())
+            return "Il nome della task non può essere vuoto."
+        if([...taskTitle].some(char => symbols.includes(char)))
+            return "Il nome della task non può contenere simboli."
+        return "";
+    }, [taskTitle]);
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        if(taskTitleError)
+            return;
+
+        const newTask = {
+            title: taskTitle.trim(),
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+
+        try{
+            await addTask(newTask);
+            alert("Task creata con successo!");
+            setTaskTitle("");
+            descriptionRef.current.value = "";
+            statusRef.current.value = "";
+        }catch(error){
+            alert(error.message);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Aggiungi una Task</h1>
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    Nome Task:
+                    <input
+                      type="text"
+                      value={taskTitle}
+                      onChange={e => setTaskTitle(e.target.value)}
+                    />
+                </label>
+                {taskTitleError &&
+                    <p style={{color: 'red'}}>{taskTitleError}</p>
+                }
+
+                <label>
+                    Descrizione:
+                    <textarea ref={descriptionRef}/>
+                </label>
+
+                <label>
+                    Stato:
+                    <select ref={statusRef} defaultValue="To do">
+                        {["To do", "Doing", "Done"].map((value, index) => (
+                            <option key={index} value={value}>{value}</option>
+                        ))}
+                    </select>
+                </label>
+                
+                <button type="submit" disabled={taskTitleError}>
+                    Aggiungi Task
+                </button>
+            </form>
+        </div>
+    )
+}
